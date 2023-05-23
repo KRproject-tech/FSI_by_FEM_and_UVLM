536e9b54a80b5de50f3e5f202e3320db110e52b8
KRproject <114337358+KRproject-tech@users.noreply.github.com>
2023-05-23 13:00:35 +0900

    Add files via upload

---
+++ single_sheet/version_check.m
@@ -0,0 +1,19 @@
+%% version
+exe_ver = 9.0;
+
+%% version check
+
+display( [ 'Solver version: ', num2str( exe_ver, '%0.1f')])
+if ~exist( 'param_ver', 'var')   
+    param_ver = nan;
+    display( 'Parameter file version: no definition')
+else    
+    display( [ 'Parameter file version: ', num2str( param_ver, '%0.1f')])
+end
+
+if exe_ver ~= param_ver
+   
+    warndlg( 'This parameter file is not compatible!!')
+end
+
+pause( 1.0);
